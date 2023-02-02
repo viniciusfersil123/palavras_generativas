157fe844cdbf10c6552aa8453ed80e02bfb04e11
Vinícius Fernandes <vinfernandes@uolinc.com>
2023-02-02 19:53:00 -0300

    chore: initial commit

---
+++ sketch.js
@@ -0,0 +1,78 @@
+let system;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  system = new ParticleSystem(createVector(width / 2, 50));
+}
+
+function draw() {
+  background(51);
+  system.origin.x = mouseX;
+  system.origin.y = mouseY;
+  if (mouseIsPressed === true) {
+    system.addParticle();
+    system.run();
+  } else {
+    // reduces quickly the lifespan of the particles
+    system.run();
+    for (let i = system.particles.length - 1; i >= 0; i--) {
+      let p = system.particles[i];
+      p.lifespan -= 5;
+      if (p.isDead()) {
+        system.particles.splice(i, 1);
+      }
+    }
+  }
+}
+
+// A simple Particle class
+let Particle = function (position) {
+  this.acceleration = createVector(0, -0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255;
+};
+
+Particle.prototype.run = function () {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function () {
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 0.5;
+};
+
+// Method to display
+Particle.prototype.display = function () {
+  noStroke();
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function () {
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function (position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function () {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function () {
+  for (let i = this.particles.length - 1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
